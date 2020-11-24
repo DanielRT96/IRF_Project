f036709a1a3ec9dfea7d3d073e6a93e40168e298
Dániel Réti <daniel.reti@stud.uni-corvinus.hu>
2020-11-24 23:19:16 +0100

    add data to the gridview

--- Project/Project/Form1.cs
+++ Project/Project/Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -15,6 +16,43 @@ namespace Project
         public Form1()
         {
             InitializeComponent();
+            LoadData();
+        }
+
+        private void LoadData()
+        {
+            DataTable dt = new DataTable();
+            string[] lines = System.IO.File.ReadAllLines("population_by_country_2020.csv");
+            if (lines.Length > 0)
+            {
+                string firstLine = lines[0];
+
+                string[] headerLabels = firstLine.Split(',');
+
+                foreach(string headerWord in headerLabels)
+                {
+                    dt.Columns.Add(new DataColumn(headerWord));
+                }
+
+                for (int i = 1; i < lines.Length; i++)
+                {
+                    string[] dataWords = lines[i].Split(',');
+                    DataRow dr = dt.NewRow();
+                    int columnIndex = 0;
+                    foreach (string headerWord in headerLabels)
+                    {
+
+                        dr[headerWord] = dataWords[columnIndex++];
+                    }
+
+                    dt.Rows.Add(dr);
+                }
+            }
+            if(dt.Rows.Count>0)
+            {
+                dataGridView1.DataSource = dt;
+            }
+
         }
     }
 }
